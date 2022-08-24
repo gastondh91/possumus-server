ce07a4213f0ae1a8004a40fa8c45d8d60d15c4b0
Gastón d'Hiriart <gastondh91@gmail.com>
2022-08-24 13:12:56 -0300

    changed variable names

--- src/services/checkpalindrome.service.ts
+++ src/services/checkpalindrome.service.ts
@@ -20,37 +20,35 @@ const checkPalindromeService = (inputText: string): CheckPalindrome => {
     )
   }
 
-  const removeRepeatedLettersFromPalindrome = (
+  const removeRepeatedSequencesFromPalindrome = (
     currentIndex: number,
     sliceIndex: number
   ) => {
-    const currentLetters = inputText.slice(currentIndex, sliceIndex)
+    const alreadyEvaluatedText = inputText.slice(0, currentIndex)
+    const currentLettersInEvaluation = inputText.slice(currentIndex, sliceIndex)
     const remainingTextLetters = inputText.slice(sliceIndex)
-    const textUntilCurrentLoop = inputText.slice(0, currentIndex)
 
-    if (!remainingTextLetters.includes(currentLetters)) return
-    const removeRepeatedLetters = remainingTextLetters.replace(
-      currentLetters,
+    if (!remainingTextLetters.includes(currentLettersInEvaluation)) return
+    const textRepeatedLettersRemoved = remainingTextLetters.replace(
+      currentLettersInEvaluation,
       ''
     )
 
-    return textUntilCurrentLoop + removeRepeatedLetters
+    return alreadyEvaluatedText + textRepeatedLettersRemoved
   }
 
   const findUnrepeatedSequencesFromPalindrome = (index: number): string => {
     // Text can only contain repeated letters by consecutive two or by consecutive three but not both.
     // First we have to check by three has three so if there's two these are not contained in the repeated three
 
-    const remainingLettersCheckingByTwo = removeRepeatedLettersFromPalindrome(
+    const remainingLettersCheckingByTwo = removeRepeatedSequencesFromPalindrome(
       index,
       index + 3
     )
     if (remainingLettersCheckingByTwo) return remainingLettersCheckingByTwo
 
-    const remainingLettersCheckingByThree = removeRepeatedLettersFromPalindrome(
-      index,
-      index + 2
-    )
+    const remainingLettersCheckingByThree =
+      removeRepeatedSequencesFromPalindrome(index, index + 2)
     if (remainingLettersCheckingByThree) return remainingLettersCheckingByThree
 
     return findUnrepeatedSequencesFromPalindrome(index + 1)
